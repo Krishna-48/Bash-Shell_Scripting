40c1d6592493af73ba4ab5937639eeeb9a40607c
Krishna-48 <105050581+Krishna-48@users.noreply.github.com>
2025-08-29 21:47:41 +0530

    Create Website_status_checker.sh

---
+++ Website_status_checker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+website="https://example.com"
+
+if curl --output /dev/null --silent --head --fail "$website";then
+        echo "website is up."
+else
+        echo "website is down."
+fi
